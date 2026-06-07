9bd5786fa58ff64680c828bfacef6339c5173b94
2410010022amat-gif <2410010022amat@gmail.com>
2026-06-08 00:42:36 +0800

    Selesai Bagian 3: Multi-Class

---
+++ src/bagian3/perpustakaan/Buku.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bagian3.perpustakaan;
+
+/**
+ *
+ * @author user
+ */
+
+public class Buku {
+    private final String judul;
+    private final String penulis;
+    private boolean dipinjam;
+    // Latihan mandiri no.2: tambah atribut ini
+    private final int tahunTerbit;
+
+    public Buku(String judul, String penulis, int tahunTerbit) {
+        this.judul = judul;
+        this.penulis = penulis;
+        this.tahunTerbit = tahunTerbit;
+        this.dipinjam = false;
+    }
+    public String getJudul()    { return judul; }
+    public String getPenulis()  { return penulis; }
+    public int getTahunTerbit() { return tahunTerbit; }
+    public boolean isDipinjam() { return dipinjam; }
+    public void setDipinjam(boolean dipinjam) { this.dipinjam = dipinjam; }
+
+    public String info() {
+        String status = dipinjam ? "Dipinjam" : "Tersedia";
+        return judul + " oleh " + penulis + " (" + tahunTerbit + ") [" + status + "]";
+    }
+}
